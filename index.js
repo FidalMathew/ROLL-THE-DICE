bf9de5a9b369e33c2021afbcd37f205abe5d0661
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-12-02 12:22:15 +0100

    Clean up code and Tuning UI

--- index.js
+++ index.js
@@ -1,125 +1,74 @@
 "use strict";
-
 let score = 0;
 let randomVar;
-
 let buttonVal = document.getElementsByTagName("button");
 let result = document.getElementById("result");
-let report = document.getElementById("report");
+let report = document.getElementById("text-init-loading");
 let timeS = document.getElementById("timeS");
 let dice = document.getElementById("dice");
-
-
-
 const Clicked = (e) => {
-    let selectVar = (e.target.innerText);
-
-    dice.src = `img/${randomVar}.png`;        //To change the dice picture           
-
-    document.getElementById("SV").innerHTML = selectVar;
-
-
-    if (randomVar == selectVar) {
-        result.style.color = "#1CE1BE";
-        result.innerHTML = "Guess was right!";
-        score++;
-        document.getElementById("CV").innerHTML = score;
-
-    }
-    else {
-        result.style.color = "#FF6D2E";
-        result.innerHTML = "Guess was wrong!";
-    }
-
-
-    Array.from(buttonVal).forEach(element => {
-        element.disabled = true;
-        element.classList.toggle("disabled");
-    });
-
+  let userSelectedNumber = (e.target.innerText);
+  dice.src = `img/${randomVar}.png`;
+  document.getElementById("text-user-selected").innerHTML = userSelectedNumber;
+  document.getElementById("text-user-selected").style.visibility="visible";
+  if (randomVar == userSelectedNumber) {
+    result.style.color = "#1CE1BE";
+    result.innerHTML = "You guess it right!";
+    score++;
+    document.getElementById("text-total-score").innerHTML = score;
+  } else {
+    result.style.color = "#FF6D2E";
+    result.innerHTML = "Sorry, it was a wrong number!";
+  }
+  Array.from(buttonVal).forEach(element => {
+    element.disabled = true;
+    element.classList.toggle("disabled");
+  });
 }
-
-
-
-
 const load = () => {
-    report.innerHTML = "loading game will start in a few seconds!";
-    document.getElementById('h').style.display = "none";
-    document.getElementById('result').style.display = "none";
-
-    Array.from(buttonVal).forEach(element => {
-
-        element.addEventListener("click", Clicked); //added event click
-
-        element.disabled = false;
-        element.classList.add("disabled");
-
-    });
-
-
-    setTimeout(() => {
-        document.getElementById('report').style.display = "none";
-        document.getElementById('h').style.display = "block";
-        document.getElementById('result').style.display = "block";
-
-
-    }, 6000);
-
+  report.innerHTML = `<h2>Loading...</h2>
+    <span style="font-size:15px;">
+      The game will start in few seconds!
+    </span>`;
+  document.getElementById('h').style.display = "none";
+  document.getElementById('result').style.display = "none";
+  document.getElementById("text-user-selected").style.visibility="hidden";
+  Array.from(buttonVal).forEach(element => {
+    element.addEventListener("click", Clicked);
+    element.disabled = false;
+    element.classList.add("disabled");
+  });
+  setTimeout(() => {
+    report.style.visibility="hidden";
+    document.getElementById('h').style.display = "block";
+    document.getElementById('result').style.display = "block";
+  }, 6000);
 }
-
-
-
 const DisplayTime = () => {
-    let timesecond = 9;
-
-    setInterval(() => {
-
-        timeS.innerHTML = timesecond;
-        timesecond--;
-        if (timesecond == 0) {
-
-            timesecond = 10;
-        }
-
-
-    }, 1000);
-
-
+  let timesecond = 9;
+  setInterval(() => {
+    timeS.innerHTML = timesecond;
+    timesecond--;
+    if (timesecond == 0) {
+      timesecond = 10;
+    }
+  }, 1000);
 }
-
-
-
-
 const addNewRandom = () => {
-
-    setInterval(() => {
-        randomVar = Math.floor(Math.random() * 6) + 1;
-
-        dice.src = `img/load.gif`;
-        dice.style.width = "37.5%";
-        result.innerHTML = "";
-    document.getElementById("SV").innerHTML = "_";
-
-
-        Array.from(buttonVal).forEach(element => {
-
-            element.disabled = false;
-            element.classList.remove("disabled");
-
-        });
-
-    }, 10000);
-
+  setInterval(() => {
+    randomVar = Math.floor(Math.random() * 6) + 1;
+    dice.src = `img/load.gif`;
+    dice.style.width = "37.5%";
+    result.innerHTML = "";
+    document.getElementById("text-user-selected").style.visibility="hidden";
+    Array.from(buttonVal).forEach(element => {
+      element.disabled = false;
+      element.classList.remove("disabled");
+    });
+  }, 10000);
 }
-
-
-
 window.onload = () => {
-
-    load();
-    DisplayTime();
-    addNewRandom();
+  load();
+  DisplayTime();
+  addNewRandom();
 }
-
-
-
